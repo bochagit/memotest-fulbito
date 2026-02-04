9b1665aa5789b0e5b9dc6c0d4d28de3dc8ff00ed
bochagit <gocardozo@alumno.unlam.edu.ar>
2026-02-04 00:41:40 -0300

    Primer commit, creando la estructura del proyecto

---
+++ main.c
@@ -0,0 +1,26 @@
+/*
+Apellido(s), nombre(s): Cardozo, Gonzalo Daniel
+DNI: 43777470
+Entrega: Sí
+
+Apellido(s), nombre(s): Bruno, Yanil
+DNI: 36992182
+Entrega: Sí
+
+Apellido(s), nombre(s): De Rogatis, Ramiro Javier
+DNI: 44005090
+Entrega: Sí
+
+Apellido(s), nombre(s): Sgro, Nicolás Agustín
+DNI: 44669267
+Entrega: Sí
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Hello world!\n");
+    return 0;
+}
